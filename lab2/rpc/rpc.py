ca9115cc7867615daedd68c2db19d0b1d5ad9b3f
dgheorghita <dangheorghita917@gmail.com>
2025-11-12 12:36:04 +0100

    Implemented Async server and client

--- lab2/rpc/rpc.py
+++ lab2/rpc/rpc.py
@@ -1,6 +1,10 @@
 import constRPC
-
+import threading
+import uuid
+import time
 from context import lab_channel
+import logging
+logger = logging.getLogger('vs2lab.lab2.rpc.server')
 
 
 class DBList:
@@ -17,20 +21,47 @@ class Client:
         self.chan = lab_channel.Channel()
         self.client = self.chan.join('client')
         self.server = None
+        self.callbacks = {}
+        self.running = False
 
     def run(self):
         self.chan.bind(self.client)
         self.server = self.chan.subgroup('server')
+        self.running = True
+        threading.Thread(target=self._listen_thread, daemon=True).start() # starts a listening Thread
 
     def stop(self):
+        self.running = False
         self.chan.leave('client')
+        
+    def _listen_thread(self):
+        while self.running:
+            msg = self.chan.receive_from_any(timeout=1)
+            if msg is None:
+                continue
 
-    def append(self, data, db_list):
+            msg_type = msg[0]
+            req_id = msg[1]
+
+            if msg_type == constRPC.ACK:
+                logger.info(f"[Client] ACK received for request {req_id}")
+            elif msg_type == constRPC.RESULT:
+                result = msg[2]
+                logger.info(f"[Client] Result received for request {req_id}")
+                if req_id in self.callbacks:
+                    cb = self.callbacks.pop(req_id)
+                    cb(result)
+            else:
+                logger.info(f"[Client] Unknown message type: {msg}")
+
+    def append(self, data, db_list, callback):
         assert isinstance(db_list, DBList)
-        msglst = (constRPC.APPEND, data, db_list)  # message payload
-        self.chan.send_to(self.server, msglst)  # send msg to server
-        msgrcv = self.chan.receive_from(self.server)  # wait for response
-        return msgrcv[1]  # pass it to caller
+        req_id = str(uuid.uuid4()) # creates request ID
+        msg = (constRPC.APPEND, req_id, data, db_list) # message payload
+        self.callbacks[req_id] = callback # register callback
+        self.chan.send_to(self.server, msg) # send msg to server
+        logger.info(f"[Client] Sent async request {req_id}")
+        return req_id
 
 
 class Server:
@@ -46,13 +77,34 @@ class Server:
 
     def run(self):
         self.chan.bind(self.server)
+        logger.info("[Server] Running...")
         while True:
-            msgreq = self.chan.receive_from_any(self.timeout)  # wait for any request
-            if msgreq is not None:
-                client = msgreq[0]  # see who is the caller
-                msgrpc = msgreq[1]  # fetch call & parameters
-                if constRPC.APPEND == msgrpc[0]:  # check what is being requested
-                    result = self.append(msgrpc[1], msgrpc[2])  # do local call
-                    self.chan.send_to({client}, result)  # return response
-                else:
-                    pass  # unsupported request, simply ignore
+            msgreq = self.chan.receive_from_any(self.timeout)
+            if msgreq is None:
+                continue
+
+            client = msgreq[0]
+            msgrpc = msgreq[1]
+            msg_type = msgrpc[0]
+
+            if msg_type == constRPC.APPEND:
+                req_id = msgrpc[1]
+                data = msgrpc[2]
+                db_list = msgrpc[3]
+
+                # send ACK 
+                self.chan.send_to({client}, (constRPC.ACK, req_id))
+                logger.info(f"[Server] ACK sent for {req_id}")
+
+                threading.Thread(
+                    target=self._handle_request,
+                    args=(client, req_id, data, db_list),
+                    daemon=True
+                ).start()
+
+    def _handle_request(self, client, req_id, data, db_list):
+        logger.info(f"[Server] Started processing request {req_id}, simulating task...")
+        time.sleep(10)
+        result = self.append(data, db_list)
+        self.chan.send_to({client}, (constRPC.RESULT, req_id, result))
+        logger.info(f"[Server] Result sent for {req_id}")
--- lab2/rpc/runcl.py
+++ lab2/rpc/runcl.py
@@ -1,16 +1,26 @@
 import rpc
 import logging
+import time
 
 from context import lab_logging
 
 lab_logging.setup(stream_level=logging.INFO)
+logger = logging.getLogger('vs2lab.lab2.rpc.runcl')
 
 cl = rpc.Client()
 cl.run()
 
 base_list = rpc.DBList({'foo'})
-result_list = cl.append('bar', base_list)
 
-print("Result: {}".format(result_list.value))
+def handle_result(result_list):
+    logger.info("Callback: Result received: {}".format(result_list.value))
+
+req_id = cl.append('bar', base_list, handle_result)
+logger.info(f"Sent async request {req_id}")
+
+
+for i in range(15):
+    logger.info(f"Client working on other tasks... {i}")
+    time.sleep(1)
 
 cl.stop()
