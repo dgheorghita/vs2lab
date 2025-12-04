e8e2c7dd3bb2283b0920021daf19b1d49a105f56
dgheorghita <dangheorghita917@gmail.com>
2025-12-04 15:08:08 +0000

    Bugfix

--- lab3/zmq4/mapper.py
+++ lab3/zmq4/mapper.py
@@ -45,8 +45,7 @@ while True:
 
     words = sentence.split()
     for word in words:
-        word = word.strip().lower().strip(string.punctuation) #cleaning
-        reducer_index = hash(word) % 2 
-        #reducer_index = int(hashlib.md5(word.encode()).hexdigest(), 16) % 2
+        word = word.lower() #cleaning
+        reducer_index = int(hashlib.md5(word.encode()).hexdigest(), 16) % 2
         reducer_sockets[reducer_index].send_string(word)
         print(f"Mapper {me} sent '{word}' to Reducer {reducer_index + 1}")
